0213274a0302933f7e7f49817a55542b3cefb6ec
Zhaociao <118403314+Iamnotsnowwhite@users.noreply.github.com>
2024-01-16 22:08:43 +0100

    Update Drachenschach.py

--- Drachenschach.py
+++ Drachenschach.py
@@ -5,7 +5,7 @@
 #{|i-i'|,|j-j'|} = {1,2} diese Bedingung simuliert genau wie ein Spinger funktioniert.
 
 n = 8
-Drachen = [0]*n
+dame = [0]*n
 
 sp    = {i:False for i in range(n)} # 行
 diag1 = {i:False for i in range(2*n-1)} # 捺 2*n-1是对角线的数量
@@ -13,42 +13,29 @@ diag2 = {i:False for i in range(-n+1,n)} # 撇
 springer = {(i,j):False for i in range(n) for j in range(n)} # ist genau wie der Schachbrett
 #erstellen die Schlüssel in dictionary
 
-def versuche(i):
-    global Aufrufe # Variable Aufrufe ist ein rekursive Prozedur 
-    Aufrufe+=1
-    if i==n:# dann ist es fertig
-        bearbeite() # geht zu Funktion bearbeite
-    else:
-        for j in range(n):
-            Drachen[i]=j #在每行格子中写上j作为value
-            versuche(i+1) #行加一
-
-def bearbeite():
-    global Anzahl
-    if keinKonflikt(): #überprüfen ob ein Konflikt existiert 
+def durchsuchen(i):
+    global Aufrufe,Anzahl #globale Variable
+    Aufrufe+=1 
+    if i==n:
         Anzahl += 1
+        print (Anzahl, dame)
+        return
+    for j in range(n):
+        dame[i]=j
+        if not (sp[j] or diag1[i+j] or diag2[i-j] or springer[(i,j)]):
+            sp[j] = diag1[i+j] = diag2[i-j] = springer[(i,j)] = True
+            durchsuchen(i+1)
+            sp[j] = diag1[i+j] = diag2[i-j] = springer[(i,j)] = False
+        
+Aufrufe=Anzahl=0 #initialisierung es beginnt bei 0 
+durchsuchen(0) # wieder beginnt bei 0 
 
-def keinKonflikt(): # hier muss was geändert werden
-    belegt_spalte = set()
-    belegt_diag1 = set()
-    belegt_diag2 = set()
-    belegt_springerstelle = set()
-    for i,j in enumerate(Drachen): # enumerate 同时访问我的key与value
-        if not (
-                teste_frei(belegt_spalte,j) and
-                teste_frei(belegt_diag1,i+j) and
-                teste_frei(belegt_diag2,i-j) and
-                teste_frei(belegt_springerstelle,((i+j+1),(i-j+2)))):
-            return False
-    return True
+print (Aufrufe,"Aufrufe.",Anzahl,"Lösungen") 
+from math import factorial
+print (f'{n}!={factorial(n)}.')
 
-def teste_frei(ss,x):
-    if x in ss: return False
-    ss.add(x) # es ist eine sotierte Menge
-    return True
-
-Aufrufe=Anzahl=0
-versuche(0)
+print ("==================================")
+print ("primitive Methode")
 
 print (Aufrufe,"Aufrufe.",Anzahl,"Lösungen")
 print (f'{n}**{n}={n**n}. {n}**{n+1}//{n-1}={n**(n+1)//(n-1)}.')
